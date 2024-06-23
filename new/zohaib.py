90dc866f4467880ebe0b74fa952843d420495aaf
Zohaib-669 <zs92051@gmail.com>
2024-06-23 15:56:59 +0500

    Add files via upload
    area of circle

---
+++ new/zohaib.py
@@ -0,0 +1,17 @@
+#first
+height=6
+base=12
+f=1
+area=f*height*base
+print("area of triangle", area)
+
+#second
+radius=5
+pie=2
+area=radius*pie
+print("area of circle", area)
+
+#third
+s=1
+area=s*s
+print("area of square", area)
